85a893358faed637e6eb9efb5f48500402403d6b
Your Name <your.email@example.com>
2024-10-09 10:33:59 +0300

    Printing Name upto 5

---
+++ src/PrinterApp.java
@@ -0,0 +1,8 @@
+public class PrinterApp {
+    public static void main(String[] args) throws Exception {
+        System.out.println("This app is made by: ");
+
+        String creater = "Darshana";
+        System.out.println(creater);
+    }
+}

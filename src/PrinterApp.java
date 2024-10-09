b58f0d748b37d75b11e9ffc0ba478b9e931b94d9
Your Name <your.email@example.com>
2024-10-09 11:04:17 +0300

    Added comments & all done

--- src/PrinterApp.java
+++ src/PrinterApp.java
@@ -1,10 +1,11 @@
 public class PrinterApp {
     public static void main(String[] args) throws Exception {
         System.out.println("This app is made by: ");
-
+        // Print App name
         String creater = "Darshana";
         System.out.println(creater);
 
+        // Insert Variables
         int number1 = 24;
         int number2 = 12;
         int multiplication = number1 * number2;
@@ -12,6 +13,7 @@ public class PrinterApp {
         int sum = number1 + number2;
         int subs = number1 - number2;
 
+        // Print the answers
         System.out.println("The value of number1 is " + number1);
         System.out.println("The value of number2 is " + number2);
         System.out.println(multiplication);
